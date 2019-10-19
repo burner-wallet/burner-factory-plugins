6a976edbe3f7c8ca976ccdc3fa86117c8797280d
David Mihal <david@dmihal.com>
2019-10-19 14:53:49 +0100

    Create test wallet

---
+++ test-wallet/src/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { xdai, dai, eth } from '@burner-wallet/assets';
+import BurnerCore from '@burner-wallet/core';
+import { InjectedSigner, LocalSigner } from '@burner-wallet/core/signers';
+import { InfuraGateway, InjectedGateway, XDaiGateway, } from '@burner-wallet/core/gateways';
+import BurnerUI from '@burner-wallet/ui';
+
+const core = new BurnerCore({
+  signers: [new InjectedSigner(), new LocalSigner()],
+  gateways: [
+    new InjectedGateway(),
+    new InfuraGateway(process.env.REACT_APP_INFURA_KEY),
+    new XDaiGateway(),
+  ],
+  assets: [xdai, dai, eth],
+});
+
+
+const BurnerWallet = () =>
+  <BurnerUI
+    core={core}
+  />
+
+
+
+ReactDOM.render(<BurnerWallet />, document.getElementById('root'));
