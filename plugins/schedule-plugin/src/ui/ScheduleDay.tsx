f16ac5e407a86d3c94754a3d4bd3900d8d7e17fb
David Mihal <david@dmihal.com>
2019-11-08 09:29:34 -0500

    Style schedule view

--- plugins/schedule-plugin/src/ui/ScheduleDay.tsx
+++ plugins/schedule-plugin/src/ui/ScheduleDay.tsx
@@ -1,15 +1,57 @@
 import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
-const ScheduleDay: React.FC<{ day: any }> = ({ day }) => {
+const DayTitle = styled.h2`
+  margin: 2px 0;
+  text-align: center;
+  background: #CCCCCC;
+  display: flex;
+  padding: 4px;
+`;
+const InnerTitle = styled.div`
+  flex: 1;
+  text-align: center;
+`;
+const TitleArrow = styled(Link)`
+  display: inline-block;
+  text-decoration: none;
+`;
+
+const Event = styled.div`
+  margin: 4px 0;
+  background: #EEEEEE;
+  padding: 8px;
+`;
+const Time = styled.div`
+  font-weight: bold;
+  color: #555555;
+`;
+const EventTitle = styled.h3`
+  margin: 0;
+`;
+
+interface ScheduleDayProps {
+  day: any;
+  previousDay?: string;
+  nextDay?: string;
+}
+
+
+const ScheduleDay: React.FC<ScheduleDayProps> = ({ day, previousDay, nextDay }) => {
   return (
     <div>
-      <div>{day.day}</div>
+      <DayTitle>
+        {previousDay && <TitleArrow to={`/schedule/${previousDay}`}>&#9664;</TitleArrow>}
+        <InnerTitle>{day.day}</InnerTitle>
+        {nextDay && <TitleArrow to={`/schedule/${nextDay}`}>&#9654;</TitleArrow>}
+      </DayTitle>
       {day.events.map((event: any) => (
-        <div>
-          <div>{event.time}</div>
-          <div>{event.title}</div>
-          <div>{event.location}</div>
-        </div>
+        <Event>
+          <Time>{event.time}</Time>
+          <EventTitle>{event.title}</EventTitle>
+          <div>{event.details}</div>
+        </Event>
       ))}
     </div>
   );
--- plugins/schedule-plugin/src/ui/ScheduleView.tsx
+++ plugins/schedule-plugin/src/ui/ScheduleView.tsx
@@ -7,9 +7,13 @@ const ScheduleView: React.FC<{ schedule: any, day?: string }> = ({ schedule, day
   return (
     <div>
       <SwipeableRoutes>
-        {schedule.map((day: any) => (
+        {schedule.map((day: any, i: number) => (
           <Route path={`/schedule/${day.day}`} key={day.day} render={() => (
-            <ScheduleDay day={day} />
+            <ScheduleDay
+              day={day}
+              previousDay={(schedule[i - 1] || {}).day}
+              nextDay={(schedule[i + 1] || {}).day}
+            />
           )} />
         ))}
       </SwipeableRoutes>
