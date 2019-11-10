d0777dcc26f6d4862c07e898d2e22626470ddea1
David Mihal <david@dmihal.com>
2019-11-10 11:20:59 -0500

    Style Order page

--- plugins/stock-market-menu-plugin/src/ui/OrdersPage.tsx
+++ plugins/stock-market-menu-plugin/src/ui/OrdersPage.tsx
@@ -1,8 +1,30 @@
 import React, { useEffect, useState } from 'react';
 import { PluginPageContext } from '@burner-wallet/types';
 import { toBN } from 'web3-utils';
+import styled from 'styled-components';
 import StockMarketMenuPlugin, { Order } from '../StockMarketMenuPlugin';
 import DrinkItem from './DrinkItem';
+
+const Order = styled.div<{ completed: boolean }>`
+  display: flex;
+  height: 50px;
+  align-items: center;
+  border-bottom: solid 1px #999999;
+  ${props => props.completed ? 'color: #cccccc' : ''}
+`;
+
+const Left = styled.div`
+  flex: 1;
+`;
+
+const Price = styled.div`
+  font-size: 24px;
+`;
+
+const Checkbox = styled.input`
+  transform: scale(1.5);
+  margin: 8px;
+`;
 
 const MenuPage: React.FC<PluginPageContext> = ({ burnerComponents, plugin, defaultAccount, actions }) => {
   const _plugin = plugin as StockMarketMenuPlugin;
@@ -15,6 +37,8 @@ const MenuPage: React.FC<PluginPageContext> = ({ burnerComponents, plugin, defau
 
   useEffect(() => {
     refresh();
+    const timeout = window.setInterval(refresh, 10 * 1000);
+    return () => window.clearTimeout(timeout);
   }, [defaultAccount]);
 
   const { Page, Button } = burnerComponents;
@@ -23,19 +47,21 @@ const MenuPage: React.FC<PluginPageContext> = ({ burnerComponents, plugin, defau
       <Button onClick={() => actions.navigateTo('/stock-market')}>Back</Button>
 
       {orders.map((order: Order) => (
-        <div key={order.tx}>
-          <div>{order.name}</div>
-          <div>{order.displayPrice}</div>
-          <div>From: {order.buyer}</div>
+        <Order key={order.tx} completed={order.completed}>
+          <Left>
+            <div>{order.name}</div>
+            <div>From: {order.buyer}</div>
+          </Left>
+          <Price>{order.displayPrice}</Price>
           {_plugin.adminMode && (
             <div>
-              <input type="checkbox" checked={order.completed} onChange={e => {
+              <Checkbox type="checkbox" checked={order.completed} onChange={e => {
                 _plugin.setCompleted(order.tx, e.target.checked);
                 refresh();
               }} />
             </div>
           )}
-        </div>
+        </Order>
       ))}
     </Page>
   );
