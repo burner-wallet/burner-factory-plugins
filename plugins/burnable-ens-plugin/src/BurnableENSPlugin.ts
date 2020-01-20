190d8a67c5e3a4697707cd311f87a19652a94614
David Mihal <david@dmihal.com>
2020-01-19 22:26:18 -0500

    Burnable ENS does reverse registration

--- plugins/burnable-ens-plugin/src/BurnableENSPlugin.ts
+++ plugins/burnable-ens-plugin/src/BurnableENSPlugin.ts
@@ -2,6 +2,13 @@ import { Plugin, BurnerPluginContext } from '@burner-wallet/types';
 import ENSPanel from './ui/ENSPanel';
 import { hash } from 'eth-ens-namehash';
 import nameTokenABI from './abi/NameToken.json';
+import reverseRegistrarABI from './abi/ReverseRegistrar.json';
+
+const reverseRegistrars: { [network: string]: string } = {
+  '1': '0x9062C0A6Dbd6108336BcBe4593a3D1cE05512069',
+  '3': '0x67d5418a000534a8F1f5FF4229cC2f439e63BBe2',
+  '5': '0xa7caBE211BFE176235aBEbF13394c4a249681709',
+};
 
 interface ConstructorParams {
   domain: string;
@@ -14,6 +21,8 @@ export default class OrderMenuPlugin implements Plugin {
   private tokenAddress: string;
   private network: string;
   private tokenContract: any;
+  private resolver: string | null = null;
+  private web3: any;
 
   constructor({ domain, tokenAddress, network = '1' }: ConstructorParams) {
     this.domain = domain;
@@ -24,8 +33,8 @@ export default class OrderMenuPlugin implements Plugin {
   initializePlugin(pluginContext: BurnerPluginContext) {
     pluginContext.addElement('home-top', ENSPanel);
 
-    const web3 = pluginContext.getWeb3(this.network);
-    this.tokenContract = new web3.eth.Contract(nameTokenABI as any, this.tokenAddress);
+    this.web3 = pluginContext.getWeb3(this.network);
+    this.tokenContract = new this.web3.eth.Contract(nameTokenABI as any, this.tokenAddress);
   }
 
   async getName(account: string) {
@@ -41,12 +50,38 @@ export default class OrderMenuPlugin implements Plugin {
   }
 
   async register(name: string, account: string) {
-    await this.tokenContract.methods.register(name).send({ from: account });
+    const [resolver, balance] = await Promise.all([
+      this.getResolver(),
+      this.web3.eth.getBalance(account),
+    ]);
+
+    const transactions = [this.tokenContract.methods.register(name).send({ from: account })];
+
+    if (balance !== '0') {
+      transactions.push(this.getReverseRegistrar().methods.claimWithResolver(account, resolver)
+        .send({ from: account }));
+    }
+    await Promise.all(transactions);
   }
 
   async burn(account: string) {
     const node = await this.tokenContract.methods.reverse(account).call();
     const token = await this.tokenContract.methods.resolveToken(node).call();
     await this.tokenContract.methods.burn(token).send({ from: account });
+  }
+
+  async getResolver() {
+    if (!this.resolver) {
+      this.resolver = await this.tokenContract.methods.resolver().call();
+    }
+    return this.resolver;
+  }
+
+  getReverseRegistrar() {
+    if (!reverseRegistrars[this.network]) {
+      throw new Error('Unsupported chain');
+    }
+
+    return new this.web3.eth.Contract(reverseRegistrarABI, reverseRegistrars[this.network]);
   }
 }
