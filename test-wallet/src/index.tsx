62a10460f4c23d1c95213263f4e95ecb789279ea
David Mihal <david@dmihal.com>
2020-01-27 23:56:20 -0500

    New version of contract wallets

--- test-wallet/src/index.tsx
+++ test-wallet/src/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { xdai, dai, eth } from '@burner-wallet/assets';
+import { xdai, dai, eth, NativeAsset } from '@burner-wallet/assets';
 import BurnerCore from '@burner-wallet/core';
 import { InjectedSigner, LocalSigner } from '@burner-wallet/core/signers';
-import { InfuraGateway, InjectedGateway, XDaiGateway, } from '@burner-wallet/core/gateways';
+import { InfuraGateway, InjectedGateway, XDaiGateway, GSNGateway } from '@burner-wallet/core/gateways';
 import ModernUI from '@burner-wallet/modern-ui';
+import ContractWalletSigner from '@burner-factory/contract-wallet-signer';
+import ContractWalletPlugin from '@burner-factory/contract-wallet-plugin';
 import PushNotificationPlugin from '@burner-factory/push-notification-plugin';
 import CollectablePlugin from '@burner-factory/collectable-plugin';
 import OrderMenuPlugin from '@burner-factory/order-menu-plugin';
@@ -13,21 +15,38 @@ import BurnableENSPlugin from '@burner-factory/burnable-ens-plugin';
 import ENSPlugin from '@burner-wallet/ens-plugin';
 import waterloo from './waterloo.json';
 
+const keth = new NativeAsset({
+  id: 'keth',
+  name: 'kETH',
+  network: '42',
+  // id: 'geth',
+  // name: 'gETH',
+  // network: '5',
+});
+
 const core = new BurnerCore({
-  signers: [new InjectedSigner(), new LocalSigner()],
+  // @ts-ignore
+  signers: [
+    new ContractWalletSigner(process.env.REACT_APP_WALLET_FACTORY_ADDRESS!, { useLocalStorage: false }),
+    new InjectedSigner(),
+    new LocalSigner(),
+  ],
   gateways: [
-    new InjectedGateway(),
+    // new InjectedGateway(),
+    new GSNGateway(),
     new InfuraGateway(process.env.REACT_APP_INFURA_KEY),
     new XDaiGateway(),
   ],
-  assets: [xdai, dai, eth],
+  assets: [keth, xdai, dai, eth],
 });
 
 
 const BurnerWallet = () =>
   <ModernUI
+    // @ts-ignore
     core={core}
     plugins={[
+      new ContractWalletPlugin(),
       new BurnableENSPlugin({
         domain: 'myburner.test',
         tokenAddress: '0xc03bbef8b85a19ABEace435431faED98c31852d9',
